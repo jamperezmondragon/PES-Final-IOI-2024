beb66c90e8eb142dc0a82d047fe25ab946196ffe
Sebryxs11 <77823421+Sebryxs11@users.noreply.github.com>
2024-06-03 02:22:49 -0600

    Create main.cpp

---
+++ problemas/Miku_en_Ecatepec/attachments/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include "Sonrisas.h"
+
+using namespace std;
+
+pair<int, int> Equipo_Sonrisas(int N, int K) {
+    pair<int, int> ans = make_pair(-1, -1);
+    return ans;
+}
