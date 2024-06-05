6ec0df933b1ceeefd73b27000cad52ae862deba5
Sebryxs11 <77823421+Sebryxs11@users.noreply.github.com>
2024-06-05 01:57:16 -0600

    Create alumnos.h

---
+++ problemas/JP_Sebas/statements/grader/alumnos.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+#include <climits>
+#include <csignal>
+#include <cstdarg>
+using namespace std;
+
+int Reto_Sebas(int N);
+int valor(int id);
