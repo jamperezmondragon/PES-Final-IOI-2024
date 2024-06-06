45666bac3914f3d3432a6c20129e142f1471ce69
Sebryxs11 <77823421+Sebryxs11@users.noreply.github.com>
2024-06-06 00:37:19 -0600

    Create sol_sub1.cpp

---
+++ problemas/JP_Sebas/solutions/sol_sub1.cpp
@@ -0,0 +1,14 @@
+#include "alumnos.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int Reto_Sebas(int N) {
+    vector<int> ar;
+    for (int i = 0; i < N; i++) {
+        ar.push_back(valor(i));
+    }
+    for (int i = 0; i < N; i++) {
+        if (ar[i] == ar[(i + N/2)%N]) return i;
+    }
+    return 0;
+}
