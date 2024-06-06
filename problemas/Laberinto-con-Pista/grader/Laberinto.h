bae94a677b87f584e2d541d255df5bbd3331b916
Sebryxs11 <77823421+Sebryxs11@users.noreply.github.com>
2024-06-06 01:21:42 -0600

    Create Laberinto.h

---
+++ problemas/Laberinto-con-Pista/grader/Laberinto.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+#include <climits>
+#include <csignal>
+#include <cstdarg>
+using namespace std;
+
+vector<int> mover(int x);
+vector<int> Pista(int N, vector<vector<int>> Laberinto);
+void Juego(vector<int> Pista);
