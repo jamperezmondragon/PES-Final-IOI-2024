6c064ab709c19b83e4d73e90842da71396b8ffbd
Sebryxs11 <77823421+Sebryxs11@users.noreply.github.com>
2024-06-06 17:24:20 -0600

    Create koala.h

---
+++ problemas/El_Flamante_Koala/grader/koala.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+#include <climits>
+#include <csignal>
+#include <cstdarg>
+using namespace std;
+
+int Flamante_Koala(int K, int N, vector<int> r1, vector<int> c1, vector<int> r2, vector<int> c2);
