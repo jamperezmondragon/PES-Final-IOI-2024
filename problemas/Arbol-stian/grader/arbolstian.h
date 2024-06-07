6a9b20a0e3102ddd613591c16d9d12e5cea804fb
Sebryxs11 <77823421+Sebryxs11@users.noreply.github.com>
2024-06-06 18:18:12 -0600

    Create arbolstian.h

---
+++ problemas/Arbol-stian/grader/arbolstian.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+#include <climits>
+#include <csignal>
+#include <cstdarg>
+using namespace std;
+
+long long Arbolstian(int N, vector<int> u, vector<int> v, vector<int> w);
