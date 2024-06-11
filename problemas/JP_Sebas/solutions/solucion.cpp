7934223a4f3fcc9a54cdd537ce2c631cd7d110a3
jpabloamezcua <139586742+jpabloamezcua@users.noreply.github.com>
2024-06-10 22:03:49 -0600

    Update solucion.cpp

--- problemas/JP_Sebas/solutions/solucion.cpp
+++ problemas/JP_Sebas/solutions/solucion.cpp
@@ -2,15 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int Reto_Sebas(int N) {
+    int n=N;
    if ((n / 2) % 2) {
 		return -1;
 	}
     if (n <= 60) {
 		for (int i = 0; i < n / 2; i++)
+        {
 			if (valor(i) == valor(i+n/2))
             {
 				return i;
 			}
+        }
 		return -1;
 	}
 	int i = 0, j = n / 2, rep = 20;
