67fc17cc67d21e23c2b92e5b8af8419355440edb
jamperezmondragon <70548653+jamperezmondragon@users.noreply.github.com>
2024-06-11 11:11:03 -0600

    Update Juan_test_gen.cpp

--- problemas/Corrupcion/Juan_test_gen.cpp
+++ problemas/Corrupcion/Juan_test_gen.cpp
@@ -3,86 +3,34 @@ using namespace std;
 
 int main() {
   vector<vector<int>> V = {{1, 1}, {1, 0}, {1, 1}};
-  int n = 1;
-  /*
-  int n = 4;
-
-
-
-
+  int n = 3;
   vector<vector<int>> C;
-  for (int i = 0; i <= size(V); i++) {
 
-    for (int j = 0; j < size(V); j++) {
+
+  for (int k = 1; k < n; k++) {
+    for (int i = 0; i <= size(V); i++) {
+
+      for (int j = 0; j < size(V); j++) {
+        C.push_back({});
+        for (int h = 0; h < size(V[j]); h++) {
+          C.back().push_back(V[j][h]); 
+        }
+        for (int h = 0; h < size(V[j]); h++) {
+          C.back().push_back(V[j][h]); 
+        }
+      }
+      if (i == size(V)) continue;
       C.push_back({});
-      for (int h = 0; h < size(V[j]); h++) {
-        C.back().push_back(V[j][h]); 
+      for (int h = 0; h < size(V[i]); h++) {
+        C.back().push_back(V[i][h]); 
       }
-      for (int h = 0; h < size(V[j]); h++) {
-        C.back().push_back(V[j][h]); 
+      for (int h = 0; h < size(V[i]); h++) {
+        C.back().push_back(0); 
       }
     }
-    if (i == size(V)) continue;
-    C.push_back({});
-    for (int h = 0; h < size(V[i]); h++) {
-      C.back().push_back(V[i][h]); 
-    }
-    for (int h = 0; h < size(V[i]); h++) {
-      C.back().push_back(0); 
-    }
+    V = C; C = {};
   }
 
-  V = C;
-  C = {};
-
-  for (int i = 0; i <= size(V); i++) {
-
-    for (int j = 0; j < size(V); j++) {
-      C.push_back({});
-      for (int h = 0; h < size(V[j]); h++) {
-        C.back().push_back(V[j][h]); 
-      }
-      for (int h = 0; h < size(V[j]); h++) {
-        C.back().push_back(V[j][h]); 
-      }
-    }
-    if (i == size(V)) continue;
-    C.push_back({});
-    for (int h = 0; h < size(V[i]); h++) {
-      C.back().push_back(V[i][h]); 
-    }
-    for (int h = 0; h < size(V[i]); h++) {
-      C.back().push_back(0); 
-    }
-  }
-
-  V = C;
-  C = {};
-
-  for (int i = 0; i <= size(V); i++) {
-
-    for (int j = 0; j < size(V); j++) {
-      C.push_back({});
-      for (int h = 0; h < size(V[j]); h++) {
-        C.back().push_back(V[j][h]); 
-      }
-      for (int h = 0; h < size(V[j]); h++) {
-        C.back().push_back(V[j][h]); 
-      }
-    }
-    if (i == size(V)) continue;
-    C.push_back({});
-    for (int h = 0; h < size(V[i]); h++) {
-      C.back().push_back(V[i][h]); 
-    }
-    for (int h = 0; h < size(V[i]); h++) {
-      C.back().push_back(0); 
-    }
-  }
-
-  V = C;
-  C = {};
-  */
   cout << 1 << endl;
 
   cout << (1<<n) << endl;
